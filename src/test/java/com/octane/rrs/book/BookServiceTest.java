6038a3997ad01f8184bfe6a64e21e56fa41da6b3
ahmedbelal8 <belal80008@gmail.com>
2024-03-05 19:58:56 +0200

    add test cases

--- src/test/java/com/octane/rrs/book/BookServiceTest.java
+++ src/test/java/com/octane/rrs/book/BookServiceTest.java
@@ -8,8 +8,12 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -29,23 +33,68 @@ public class BookServiceTest {
     }
 
     @Test
+    @WithMockUser(username = "bm@bm.com", roles = "ADMIN")
     public void testUpdateIntervals() {
         // Mock data
         UserReadingInterval newUserReadingInterval = UserReadingInterval.builder().startPage(4).endPage(10).build();
         int userId = 456;
         int bookId = 123;
-
-
         // Mock repository behavior
         when(repository.findAllByUserIdAndBookIdOrderByStartPage(userId, bookId)).thenReturn(new ArrayList<>());
 
         int result = userReadingIntervalService.updateIntervals(newUserReadingInterval, bookId, userId);
 
         // Assert the result
-        assertEquals(6, result);
+        assertEquals(7, result);
 
         // Verify method calls
         verify(repository).findAllByUserIdAndBookIdOrderByStartPage(anyInt(), anyInt());
 
     }
+
+    @Test
+    public void testUpdateIntervals_ExistingIntervals() {
+        // Mock data
+        UserReadingInterval newUserReadingInterval = UserReadingInterval.builder().startPage(4).endPage(10).build();
+        int userId = 456;
+        int bookId = 123;
+
+        // Mock existing intervals
+        List<UserReadingInterval> existingIntervals = new LinkedList<>(Arrays.asList(
+                UserReadingInterval.builder().startPage(1).endPage(3).build(),
+                UserReadingInterval.builder().startPage(11).endPage(15).build()
+        ));
+        when(repository.findAllByUserIdAndBookIdOrderByStartPage(userId, bookId)).thenReturn(existingIntervals);
+
+        int result = userReadingIntervalService.updateIntervals(newUserReadingInterval, bookId, userId);
+
+        // Assert the result
+        assertEquals(7, result);
+
+        // Verify method calls
+        verify(repository).findAllByUserIdAndBookIdOrderByStartPage(anyInt(), anyInt());
+    }
+
+    @Test
+    public void testUpdateIntervals_OverlapWithExistingIntervals() {
+        // Mock data
+        UserReadingInterval newUserReadingInterval = UserReadingInterval.builder().startPage(4).endPage(10).build();
+        int userId = 456;
+        int bookId = 123;
+
+        // Mock existing intervals
+        List<UserReadingInterval> existingIntervals = new LinkedList<>(Arrays.asList(
+                UserReadingInterval.builder().id(1).bookId(bookId).userId(userId).startPage(1).endPage(5).build(),
+                UserReadingInterval.builder().id(2).bookId(bookId).userId(userId).startPage(8).endPage(12).build()
+        ));
+        when(repository.findAllByUserIdAndBookIdOrderByStartPage(userId, bookId)).thenReturn(existingIntervals);
+        int result = userReadingIntervalService.updateIntervals(newUserReadingInterval, bookId, userId);
+
+        // Assert the result
+        assertEquals(2, result);
+
+        // Verify method calls
+        verify(repository).findAllByUserIdAndBookIdOrderByStartPage(anyInt(), anyInt());
+    }
+
 }
